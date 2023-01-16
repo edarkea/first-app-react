548049cfc21f782aa9ea8a08c2f390cdc1bddfe3
Edinson Ayui <edinson.ayui@gmail.com>
2023-01-16 11:52:21 -0500

    Remove folder components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import './App.css';
-import Customer from './components/Customer';
-import { CustomerClass } from './components/CustomerClass';
-
-import Employee from './components/Employee';
-import { EmployeeClass } from './components/EmployeeClass';
 
 function App() {
   return (
@@ -18,26 +13,6 @@ function App() {
               <button className="btn btn-success btn-sm">
                 <i className='fa fa-book'></i> Read More
               </button>
-            </div>
-          </div>
-          {/* <div className="row">
-            <div className="col">
-              <Customer name={"edinson"} age={27} title={"Sistemas"} />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <CustomerClass name={"edinson"} age={27} title={"Sistemas"}/>
-            </div>
-          </div> */}
-          <div className="row">
-            <div className="col">
-              <Employee/>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <EmployeeClass/>
             </div>
           </div>
         </div>
